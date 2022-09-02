0c827f2c79926236a1ac5755fb5c13986e08233e
niveksan <werbekev@hotmail.com>
2022-09-02 15:06:30 +0200

    modify Nextcloud destination

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-rclone sync Local:data/bitwarden_rs-local-backup/backups Nextcloud:Bitwarden
+rclone sync Local:/data/bitwarden_rs-local-backup/backups Nextcloud:/Bitwarden
 # rclone sync Local:/data Nextcloud:
