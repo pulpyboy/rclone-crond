9d728e385917693b1a58b085c9a8031995a29005
niveksan <werbekev@hotmail.com>
2022-09-02 15:14:04 +0200

    adding Nextcloud destination for Home Assistant

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-
+#Bitwarden
 rclone sync Local:/data/bitwarden_rs-local-backup/backups Nextcloud:/Bitwarden
 # rclone sync Local:/data Nextcloud:
+
+#Home Assistant
+rclone sync Local:/data/homeassistant/backups Nextcloud:/Home_Assistant
