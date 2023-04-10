1d7792cadb5b72dd8591295cd687db4a36e81f55
niveksan <werbekev@hotmail.com>
2023-04-10 15:57:35 +0200

    switch from sync to move

--- backup.sh
+++ backup.sh
@@ -7,4 +7,4 @@ rclone sync -v --stats-one-line Local:/data/bitwarden_rs-local-backup/backups Ne
 rclone sync -v --stats-one-line Local:/data/homeassistant/backups Nextcloud:/Home_Assistant >> /proc/1/fd/1 2>&1
 
 #Nextcloud
-rclone sync -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
+rclone move -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
