08a3b52086a755fd523138686ac69883c681200e
niveksan <werbekev@hotmail.com>
2023-12-04 17:00:50 +0100

    remove bandwith limit

--- backup.sh
+++ backup.sh
@@ -7,4 +7,6 @@ rclone sync -v --stats-one-line Local:/data/bitwarden_rs-local-backup/backups Ne
 rclone sync -v --stats-one-line Local:/data/homeassistant/backups Nextcloud:/Home_Assistant >> /proc/1/fd/1 2>&1
 
 #Nextcloud
-rclone move -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
+#rclone move -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
+#ohne Limit
+rclone move -v --stats-one-line Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
