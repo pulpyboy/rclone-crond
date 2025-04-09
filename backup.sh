eaf5336913f75c4ab0491859e82736004c610b65
pulpyboy <35870332+pulpyboy@users.noreply.github.com>
2025-04-09 19:57:25 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-#Bitwarden
-rclone sync -v --stats-one-line Local:/data/bitwarden_rs-local-backup/backups Nextcloud:/Bitwarden >> /proc/1/fd/1 2>&1
+#Ark-Office Data
+rclone sync -v --stats-one-line ark-ofc: /data/Ark-Office >> /proc/1/fd/1 2>&1
 
-#Home Assistant
-rclone sync -v --stats-one-line Local:/data/homeassistant/backups Nextcloud:/Home_Assistant >> /proc/1/fd/1 2>&1
+#Ankit-Data
+rclone sync -v --stats-one-line ankit-od: /data/Ankit-od >> /proc/1/fd/1 2>&1
 
-#Nextcloud
-#rclone move -v --stats-one-line --bwlimit=800k:off Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
-#ohne Limit
-rclone move -v --stats-one-line Local:/data/nextcloud/backups Nextcloud:/Nextcloud >> /proc/1/fd/1 2>&1
+#Sunny-Data
+rclone move -v --stats-one-line sunny-od: /data/Sunny-od >> /proc/1/fd/1 2>&1
